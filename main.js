f3b1c02c2f4e03295ed0be568cdc71a10bf71db6
Juan José Inzunza Rivas <j.inzunza05@ufromail.cl>
2026-06-17 13:00:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ async function cargar(id, archivo) {
   document.getElementById(id).innerHTML = await respuesta.text();
 }
 
+cargar("mainTitle", "Componentes/mainTitle.html");
 cargar("boarding", "Componentes/boarding.html");
 cargar("boarding2", "Componentes/boarding2.html");
 cargar("infoTitle", "Componentes/infoTitle.html");
 cargar("firstInfo", "Componentes/firstInfo.html");
+cargar("simulator", "Componentes/sim.html");
